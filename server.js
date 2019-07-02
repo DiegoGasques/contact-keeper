05561461037d0fc7d2e6ec0b1d08d2783938b7d2
Diego <diegomgasques@gmail.com>
2019-07-02 13:53:22 -0300

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello World " });
-});
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 connectDB(() => {
